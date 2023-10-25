5549cf4050cac7ce89409d71358dfded9e42d43e
adgholson <adgholson98@gmail.com>
2023-10-25 15:59:50 -0600

    Footer
    created footer and added some styles

---
+++ client/src/components/Footer.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <nav className="footer">
+            <div className="footer-left">
+                <img className="footer-logo" src="..\LDLogoBang_NoWhite.png" alt="Logo" />
+                <span className="footer-title">Created By Aaron Gholson</span>
+            </div>
+            <div className="footer-right">
+                <ul className="footer-links">
+                    <a href="https://www.youtube.com/channel/UCaYBxVkAgdgjb06GvErd3NQ" target="_blank" rel="noopener noreferrer">
+                    <i class="bi bi-youtube"></i>
+                    </a>
+                    <a href="https://twitter.com/LegendDairyProd" target="_blank" rel="noopener noreferrer">
+                    <i class="bi bi-twitter"></i>
+                    </a>
+                    <a href="https://www.instagram.com/legenddairyprod/" target="_blank" rel="noopener noreferrer">
+                    <i class="bi bi-instagram"></i>
+                    </a>
+                    <a href="https://www.tiktok.com/@legenddairyprod" target="_blank" rel="noopener noreferrer">
+                    <i class="bi bi-tiktok"></i>
+                    </a>
+                    <a href="https://www.reddit.com/user/LegendDairyProd" target="_blank" rel="noopener noreferrer">
+                    <i class="bi bi-reddit"></i>
+                    </a>
+                    <div className="footer-divider"></div>
+                    <div className="footer-right-div">
+                        <a href="https://github.com/adgholson" target="_blank" rel="noopener noreferrer">
+                        <i class="bi bi-github"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/adgholson/" target="_blank" rel="noopener noreferrer">
+                        <i class="bi bi-linkedin"></i>
+                        </a>
+                    </div>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Footer;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './Navbar.css';
 import { Button } from "react-bootstrap";
 
-const NavBar = () => {
+const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="navbar-left">
@@ -13,19 +13,16 @@ const NavBar = () => {
             <div className="navbar-right">
                 <ul className="navbar-links">
                     <li>
-                        <h3 className="navlink">Page1</h3>
+                        <h3 className="navlink">Home</h3>
                     </li>
                     <li>
-                        <h3 className="navlink">Page2</h3>
-                    </li>
-                     <li>
-                        <h3 className="navlink">Page3</h3>
+                        <h3 className="navlink">Games</h3>
                     </li>
                 </ul>
-                <Button className="login-button">Login</Button>
+                <Button className="login-button">Sign Up</Button>
             </div>
         </nav>
     );
 };
 
-export default NavBar;
+export default Navbar;
