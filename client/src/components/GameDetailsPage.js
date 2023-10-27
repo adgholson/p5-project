58c0596fd0ed62beaeadcc74676a674f2dc386ec
adgholson <adgholson98@gmail.com>
2023-10-27 17:08:48 -0600

    Review-Setup
    set up the review feature and linked it with users and games, right now it works in theory

--- client/src/components/GameDetailsPage.js
+++ client/src/components/GameDetailsPage.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import VideoBackground from "./VideoBackground";
 import { Card } from "react-bootstrap";
 import "./GameDetailsPage.css";
+import ReviewForm from "./ReviewForm";
 
-const GameDetailsPage = ({ games }) => {
+const GameDetailsPage = ({ games, user }) => {
   const { gameId } = useParams();
   const game = games.find((game) => game.id === parseInt(gameId, 10));
+  const [reviews, setReviews] = useState(game.reviews || []);
+
+  const handleReviewSubmit = (newReview) => {
+    setReviews([...reviews, newReview]);
+  };
 
   if (!game) {
     return <div>Game not found</div>;
@@ -16,7 +22,7 @@ const GameDetailsPage = ({ games }) => {
     <div className="game-page">
         <VideoBackground />
         <div className="game-container">
-            <div>
+        <div className="content-container">
             <Card className="game-card">
                 <Card.Img variant="top" src={game.cover_image} alt={game.title} />
                 <Card.Body className="card-body">
@@ -26,9 +32,19 @@ const GameDetailsPage = ({ games }) => {
                 <Card.Text>Platforms: {game.platforms}</Card.Text>
                 </Card.Body>
             </Card>
+            <ReviewForm gameId={gameId} user={user} onReviewSubmit={handleReviewSubmit} />
+          </div>
+            <div className="reviews-list">
+            <h2 className="reviews-title">Reviews!</h2>
+            {reviews.map((review) => (
+              <div key={review.id}>
+                <strong>{review.user.username}</strong>: {review.content} (Rating: {review.rating})
+              </div>
+            ))}
+          </div>
             </div>
         </div>
-    </div>
+
   );
 };
 
