8fe134bdbbdac9ba3ef23bc824f4a9796422165d
adgholson <adgholson98@gmail.com>
2023-11-06 10:32:33 -0700

    Finalized-Noti-System
    Finished notification system with styling

--- client/src/components/Notification.js
+++ client/src/components/Notification.js
@@ -2,16 +2,17 @@ import React, { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import './Notification.css';
 
-function Notification({favoriteGameId, title}) {
+function Notification({ favoriteGameId, title }) {
   const [notificationCount, setNotificationCount] = useState(0);
+  const [notificationMessages, setNotificationMessages] = useState([]);
 
   useEffect(() => {
-    
     if (favoriteGameId) {
       const eventSource = new EventSource(`http://localhost:5555/stream/${favoriteGameId}/reviews`);
       eventSource.onmessage = (message) => {
-        console.log(message)
+        const newNotification = "A new review was posted!";
         setNotificationCount(prevCount => prevCount + 1);
+        setNotificationMessages(prevMessages => [...prevMessages, newNotification]);
       };
 
       eventSource.onerror = (error) => {
@@ -27,11 +28,17 @@ function Notification({favoriteGameId, title}) {
 
   const clearNotifications = () => {
     setNotificationCount(0);
+    setNotificationMessages([]);
   };
 
   return (
     <div className="noti-div">
-      <h2 className="noti-count">Updates in {title}: {notificationCount}</h2>
+      <h2 className="noti-header">Updates in {title}: <h3 className="noti-count-h3">{notificationCount}</h3></h2>
+      <div className="noti-list">
+        {notificationMessages.map((message, index) => (
+          <div key={index} className="noti-message">{message}</div>
+        ))}
+      </div>
       <Button className="noti-button" onClick={clearNotifications}>Clear Notifications</Button>
     </div>
   );
